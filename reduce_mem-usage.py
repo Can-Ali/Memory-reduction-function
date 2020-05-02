a567b94be8e95e9293246f8e0c142c162bdc9365
Berkan Acar <61150565+berkarcan@users.noreply.github.com>
2020-05-02 23:40:41 +0200

    Update reduce_mem-usage.py

--- reduce_mem-usage.py
+++ reduce_mem-usage.py
@@ -1,6 +1,4 @@
 """
-https://www.kaggle.com/gemartin/load-data-reduce-memory-usage
-
 Note: If you have problems with this function try to discard Float16 part as
 there might be errors because of this data type:
 https://github.com/pandas-dev/pandas/issues/9220
